f5be09711a2304994590f4bd48f488d7fd87f165
Imam Hanafi <52751983+imamhanafi1998@users.noreply.github.com>
2023-11-17 16:35:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,35 @@ const io = require('socket.io')(server, {
 const port = process.env.PORT || 5000;
 
 io.on('connection',(socket)=>{
-    console.log('client connected: ',socket.id)
+    console.log('client connected: ', socket.id)
     
-    socket.join('clock-room')
+    // socket.emit('newUser', socket.id)
+    
+    // socket.join('clock-room')
+    // socket.join('new-user')
+    
+    // socket.emit("foo", "test");
+    // socket.on("bar", (data) => {
+    //     console.log(`bar ${data} received`);
+    // });
+    socket.on('joined', id => {
+        console.log(`${id} has joined `);
+        // socket.broadcast.emit('userJoined',`${id} has joined`);
+        socket.broadcast.emit('userJoined', id);
+        // socket.emit('welcome', id)
+    })
     
     socket.on('disconnect',(reason)=>{
-        console.log(reason)
+        // console.log(reason)
+        console.log('client disconnected: ', socket.id);
     })
 })
-setInterval(()=>{
-    io.to('clock-room').emit('time', new Date())
-},1000)
+
+// io.to('new-user').emit('tg', 'new connected')
+// socket.emit("hello", "world");
+// setInterval(()=>{
+//     io.to('clock-room').emit('time', 'test')
+// },1000)
 server.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
